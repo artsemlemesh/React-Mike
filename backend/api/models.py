377700847645183187f8db78545a1020bc47d825
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-24 14:14:53 +0800

    m

---
+++ backend/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
