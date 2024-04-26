cfe85b271f98073131e0acd67c5ea079a4c3c39e
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-27 06:52:16 +0800

    d

--- REACT_25/React-int/src/scroll-indicator/index.jsx
+++ REACT_25/React-int/src/scroll-indicator/index.jsx
@@ -1,80 +1,80 @@
-import { useState, useEffect } from "react";
-import './scroll.css'
+// import { useState, useEffect } from "react";
+// import './scroll.css'
 
 
-export default function ScrollIndicator({ url }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+// export default function ScrollIndicator({ url }) {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
 
-  //for scroll indicator
-  const [scrollPercent, setScrollPercent] = useState(0);
+//   //for scroll indicator
+//   const [scrollPercent, setScrollPercent] = useState(0);
 
-  async function fetchData(getUrl) {
-    try {
-      setLoading(true);
-      const response = await fetch(getUrl);
-      const data = await response.json();
+//   async function fetchData(getUrl) {
+//     try {
+//       setLoading(true);
+//       const response = await fetch(getUrl);
+//       const data = await response.json();
 
-      if (data && data.products && data.products.length > 0) {
-        setData(data.products);
-        setLoading(false);
-      }
-    } catch (e) {
-      console.log(e);
-      setErrorMessage(e.message);
-    }
-  }
+//       if (data && data.products && data.products.length > 0) {
+//         setData(data.products);
+//         setLoading(false);
+//       }
+//     } catch (e) {
+//       console.log(e);
+//       setErrorMessage(e.message);
+//     }
+//   }
 
-  useEffect(() => {
-    fetchData(url);
-  }, [url]);
+//   useEffect(() => {
+//     fetchData(url);
+//   }, [url]);
 
-  function handleScrollPercent() {
-    const howMuchScrolled =
-      document.body.scrollTop || document.documentElement.scrollTop;
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
+//   function handleScrollPercent() {
+//     const howMuchScrolled =
+//       document.body.scrollTop || document.documentElement.scrollTop;
+//     const height =
+//       document.documentElement.scrollHeight -
+//       document.documentElement.clientHeight;
 
-    setScrollPercent((howMuchScrolled / height) * 100);
-  }
+//     setScrollPercent((howMuchScrolled / height) * 100);
+//   }
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScrollPercent);
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScrollPercent);
 
-    return () => {
-      window.removeEventListener("scroll", () => {});
-    };
-  });
+//     return () => {
+//       window.removeEventListener("scroll", () => {});
+//     };
+//   });
 
-  console.log(data, scrollPercent);
+//   console.log(data, scrollPercent);
 
-  if(errorMessage){
-    return <div>error! {errorMessage}</div>
-  }
+//   if(errorMessage){
+//     return <div>error! {errorMessage}</div>
+//   }
 
-  if(loading){
-    return <div>loading data wait</div>
-  }
+//   if(loading){
+//     return <div>loading data wait</div>
+//   }
 
-  return (
-    <div>
-      <div className="top-container">
-        <h1>custom scroll indicator</h1>
-        <div className="scroll-progress-tracking-container">
-          <div
-            className="current-progress-bar"
-            style={{ width: `${scrollPercent}%` }}
-          ></div>
-        </div>
-      </div>
+//   return (
+//     <div>
+//       <div className="top-container">
+//         <h1>custom scroll indicator</h1>
+//         <div className="scroll-progress-tracking-container">
+//           <div
+//             className="current-progress-bar"
+//             style={{ width: `${scrollPercent}%` }}
+//           ></div>
+//         </div>
+//       </div>
 
-      <div className="data-container">
-        {data && data.length > 0
-          ? data.map((dataItem) => <p>{dataItem.title}</p>)
-          : null}
-      </div>
-    </div>
-  );
-}
+//       <div className="data-container">
+//         {data && data.length > 0
+//           ? data.map((dataItem) => <p>{dataItem.title}</p>)
+//           : null}
+//       </div>
+//     </div>
+//   );
+// }
