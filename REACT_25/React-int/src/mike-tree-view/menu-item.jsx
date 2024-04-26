b30e7df2c691e60f1a68852b5462e658a393f790
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-26 11:49:16 +0800

    d

--- REACT_25/React-int/src/mike-tree-view/menu-item.jsx
+++ REACT_25/React-int/src/mike-tree-view/menu-item.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import MenuListNew from "./menu-list"
-
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 export default function MenuItemNew({item}) {
     const [currChild, setCurrChild] = useState({});
@@ -9,24 +9,25 @@ export default function MenuItemNew({item}) {
 
     function toggleChildren(getCurrentLabel){
         setCurrChild({
-            ...currChild,
-            [getCurrentLabel]: !currChild[getCurrentLabel],
+            ...currChild, //copies the current state
+            [getCurrentLabel]: !currChild[getCurrentLabel], // toggles current label to true/false(show/hide)
         })
     }
 
     return (
         <li>
             <div className="menu-item">
-                <p>{item.label}</p>
+                
+                <p>{item.label}</p>  {/* displays the tab's name */}
                 {item && item.children && item.children.length ? (
-                    <span onClick={()=> toggleChildren(item.label)}>
-                        {}
+                    <span onClick={()=> toggleChildren(item.label)}> {/* click, calls function that toggles current label */}
+                        {currChild[item.label] ? <FaMinus color="#fff" size={25}/> : <FaPlus color='#fff' size={25}/>} {/* dynamic + and - */}
                     </span>
                 ) : null}
             </div>
 
-            {item && item.children && item.children.length > 0 && currChild[item.label] ? (
-                <MenuListNew list={item.children} />
+            {item && item.children && item.children.length > 0 && currChild[item.label] ? ( // if there are children and currChild[item.label] is true, then MenuListNew calls again to display labels down the tree
+                <MenuListNew list={item.children} />    //recursive funciton
             ) : null}
         </li>
     )
