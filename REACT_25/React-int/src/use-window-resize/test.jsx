dd0b2e9329bab4a02dfbb847084261dfe5eed78e
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-30 15:01:45 +0800

    d

--- REACT_25/React-int/src/use-window-resize/test.jsx
+++ REACT_25/React-int/src/use-window-resize/test.jsx
@@ -1,10 +1,26 @@
 import useWindowResize from ".";
 
-export default function UseWindowResizeTest() {
-  const windowSize = useWindowResize();
-  const { width, height } = windowSize;
+// export default function UseWindowResizeTest() {
+//   const windowSize = useWindowResize();
+//   const { width, height } = windowSize;
 
-  return (
+//   return (
+//     <div>
+//       <h1>use window resize hook</h1>
+//       <p>width is {width}</p>
+//       <p>height is {height}</p>
+//     </div>
+//   );
+// }
+
+export default function UseWindowResizeTest() {
+  
+  const windowSize = useWindowResize()
+  const {width, height} = windowSize;
+
+  
+  
+    return (
     <div>
       <h1>use window resize hook</h1>
       <p>width is {width}</p>
