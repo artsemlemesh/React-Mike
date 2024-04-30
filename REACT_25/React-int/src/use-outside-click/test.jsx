afd7f135ce3fe6902e177a1a73d621f0adc2e03e
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-30 14:18:44 +0800

    d

--- REACT_25/React-int/src/use-outside-click/test.jsx
+++ REACT_25/React-int/src/use-outside-click/test.jsx
@@ -1,51 +1,29 @@
-// import { useRef, useState } from "react";
-// import useOutsideClick from ".";  //path to the custom hook
-
-// export default function UseOnclickOutsideTest() {
-//     const [showContent, setShowContent] = useState(false);
-
-//     const ref = useRef()//creates a reference to a DOM element
-//     //useRef is a hook that is used for a value that is not needed to be rerendered
-
-//     //call useOutsideClick hook to hide content when clicked outside
-//     useOutsideClick(ref, ()=> setShowContent(false))
-
-//   return (
-//     <div>
-//       {showContent ? (
-//         <div ref={ref}>
-//             <h1>this is a random content</h1>
-//             <p>click outside</p>
-//         </div>
-//       ) : (
-//         <button onClick={() => setShowContent(true)}>Show content</button>
-//       )}
-//     </div>
-//   );
-// }
-
-// //.focus() automatically focuses on an input line, for mobiles it triggers a keyboard, very helpful and useful
-
 import { useRef, useState } from "react";
-
-import useOutsideClick from ".";
+import useOutsideClick from ".";  //path to the custom hook
 
 export default function UseOnclickOutsideTest() {
-  const [showContent, setShowContent] = useState(false);
-  const ref = useRef(); //creates a reference to a DOM element
+    const [showContent, setShowContent] = useState(false);
 
-  useOutsideClick(ref, () => setShowContent(false));
+    const ref = useRef()//creates a reference to a DOM element
+    //useRef is a hook that is used for a value that is not needed to be rerendered
+
+    //call useOutsideClick hook to hide content when clicked outside
+    useOutsideClick(ref, ()=> setShowContent(false))
 
   return (
     <div>
       {showContent ? (
         <div ref={ref}>
-          <h1>test title</h1>
-          <p>click outside this area</p>
+            <h1>this is a random content</h1>
+            <p>click outside</p>
         </div>
       ) : (
-        <button onClick={() => setShowContent(true)}>show content</button>
+        <button onClick={() => setShowContent(true)}>Show content</button>
       )}
     </div>
   );
 }
+
+//.focus() automatically focuses on an input line, for mobiles it triggers a keyboard, very helpful and useful
+
+
