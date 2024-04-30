8c636a78a7ae3e8e1bb8afba442d73023c8b1569
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-30 16:59:44 +0800

    d

--- REACT_25/React-int/src/scroll-to-particular-section/scroll-to-section.jsx
+++ REACT_25/React-int/src/scroll-to-particular-section/scroll-to-section.jsx
@@ -1,75 +1,71 @@
-import { useRef } from "react"
+import { useRef } from "react";
 
+export default function ScrollToSection() {
+  const ref = useRef();
 
-export default function ScrollToSection(){
+  const data = [
+    {
+      label: "first card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "red",
+      },
+    },
+    {
+      label: "second card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "blue",
+      },
+    },
+    {
+      label: "third card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "yellow",
+      },
+    },
+    {
+      label: "fourth card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "grey",
+      },
+    },
+    {
+      label: "fifth card",
+      style: {
+        width: "100%",
+        height: "600px",
+        background: "pink",
+      },
+    },
+  ];
 
-    const ref = useRef();
+  function scrollToSection() {
+    let pos = ref.current.getBoundingClientRect().top;//distance btw top of the viewport and the ref section
 
-    const data = [
-        {
-            label: 'first card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'red',
-            }
-        },
-        {
-            label: 'second card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'blue',
-            }
-        },
-        {
-            label: 'third card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'yellow',
-            }
-        },
-        {
-            label: 'fourth card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'grey',
-            }
-        },
-        {
-            label: 'fifth card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'pink',
-            }
-        },
-    ]
+    window.scrollTo({
+      top: pos,
+      behavior: "smooth",
+    });
+  }
 
-    function scrollToSection(){
-        let pos = ref.current.getBoundingClientRect().top
+  return (
+    <div>
+      <h1>scroll to a partic sections</h1>
 
-        window.scrollTo({
-            top: pos,
-            behavior: 'smooth'
-        })
-    }
-
-
-
-    
-
-    return <div>
-        <h1>scroll to a partic sections</h1>
-
-        <button onClick={scrollToSection}>click to scroll</button>
-        {
-            data.map((item, index) => <div ref={index === 2 ? ref : null} style={item.style}>
-                <h3>{item.label}</h3>
-            </div>)
-        }
-
+      <button onClick={scrollToSection}>click to scroll</button>
+      {data.map((item, index) => (
+        <div ref={index === 2 ? ref : null} style={item.style}>
+          <h3>{item.label}</h3>
+        </div>
+      ))}
     </div>
+  );
 }
+
