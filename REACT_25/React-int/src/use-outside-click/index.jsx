e5bb53024fadb678d871c057e26bbb2e17c9ed2e
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-04-30 14:09:57 +0800

    d

--- REACT_25/React-int/src/use-outside-click/index.jsx
+++ REACT_25/React-int/src/use-outside-click/index.jsx
@@ -1,26 +1,26 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// export default function useOutsideClick(ref, handler) {
-//         // ref- object that references the DOM element I want to detect clicks outside of
-//         //handler- function called when i clicked outside the ref element
-//   useEffect(() => {
-//     //useEffect to set up event listeners when the component mounts and remove when unmounts
-//     function listener(e) {//checks for clicks (because of e)
-//       if (!ref.current || ref.current.contains(e.target)) {//if clicked element is outside of the specified element, it calls the handler func
-//         return;//ignore clicks inside the specified element
-//       }
-//       //call the provided handler function when clicked outside
-//       handler(e);
-//     }
+export default function useOutsideClick(ref, handler) {
+        // ref- object that references the DOM element I want to detect clicks outside of
+        //handler- function called when i clicked outside the ref element
+  useEffect(() => {
+    //useEffect to set up event listeners when the component mounts and remove when unmounts
+    function listener(e) {//checks for clicks (because of e)
+      if (!ref.current || ref.current.contains(e.target)) {//if clicked element is outside of the specified element, it calls the handler func
+        return;//ignore clicks inside the specified element
+      }
+      //call the provided handler function when clicked outside
+      handler(e);
+    }
 
-//     document.addEventListener("mousedown", listener);
-//     document.addEventListener("touchstart", listener);
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
 
-//     return () => {//remove event listeners to prevent memory leaks
-//       document.removeEventListener("mousedown", listener);
-//       document.removeEventListener("touchstart", listener);
-//     };
-//   }, [handler, ref]);
-// }
+    return () => {//remove event listeners to prevent memory leaks
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [handler, ref]);
+}
 
 
