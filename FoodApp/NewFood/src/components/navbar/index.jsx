d8ba78a1f1daabe48faec7fec2c47dc891ea967d
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-05-02 11:56:05 +0800

    d

--- FoodApp/NewFood/src/components/navbar/index.jsx
+++ FoodApp/NewFood/src/components/navbar/index.jsx
@@ -1,7 +1,18 @@
+import '../../index.css'
 
 
-
-export default function Navbar(){
-
-    return <div>Navbar</div>
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold"></h2>
+      <form>
+      <input
+          type="text"
+          name="search"
+          placeholder="Enter Items..."
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        />
+      </form>
+    </nav>
+  );
 }
--- FoodApp/NewFood/src/main.jsx
+++ FoodApp/NewFood/src/main.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/index.jsx";
 import Favourites from "./pages/favourites/index.jsx";
 import Details from "./pages/details/index.jsx";
-import Navbar from './components/navbar/index.jsx';
-import './index.css'
+import Navbar from "./components/navbar/index.jsx";
+import GlobalState from './context'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home/>
+    path: "/",
+    element: <Home />,
   },
   {
-    path: '/fav',
-    element: <Favourites/>
+    path: "/fav",
+    element: <Favourites />,
   },
   {
-    path: '/recipe-item/:id',
-    element: <Details/>
-  }
+    path: "/recipe-item/:id",
+    element: <Details />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar/>
-  <RouterProvider router={router}/>
-    {/* <App /> */}
-  </React.StrictMode>,
-)
+    <GlobalState>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar/>
+        <RouterProvider router={router} />
+      </div>
+    </GlobalState>
+  </React.StrictMode>
+);
