803077e9a1254a86ef759fe522e643f5045e9286
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-05-12 15:55:22 +0800

    d

--- NoTutorial/src/pages/navbar.jsx
+++ NoTutorial/src/pages/navbar.jsx
@@ -1,30 +1,30 @@
 import { NavLink, Outlet } from "react-router-dom";
-import '../index.css'
+import "../index.css";
+import StatusBar from "./two";
 
-
-const links = [
-    'one', 'two', 'three'
-]
-
+const links = ["one", "two", "three"];
 
 const Navbar = () => {
-
-
   return (
     <>
       <nav>
-        <h1 className="navb">This is my navbar</h1>
+        <h1 className="navb">
+          <div>This is my navbar</div>
+          <div className="stat">
+            <StatusBar />
+          </div>
+        </h1>
+
         <div className="navlinkdiv">
-            
-        <NavLink to={"/fav"}>Favorites</NavLink>
-        <NavLink to={"/about"}>About</NavLink>
-        <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/fav"}>Favorites</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/"}>Home</NavLink>
         </div>
-        {
-            links.map((item, index) => <NavLink key={index} to={`/${item}`}>
-                {item}
-            </NavLink>)
-        }
+        {links.map((item, index) => (
+          <NavLink key={index} to={`/${item}`}>
+            {item}
+          </NavLink>
+        ))}
       </nav>
     </>
   );
