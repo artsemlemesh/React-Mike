746b954c67e5bc765841edb36b667596538ce5bc
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-05-13 14:43:14 +0800

    d

--- NoTutorial/src/pages/navbar.jsx
+++ NoTutorial/src/pages/navbar.jsx
@@ -1,17 +1,24 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "../index.css";
 import StatusBar from "./two";
+import { useState } from "react";
+import useLS from "../components/navbarCustomHook";
 
 const links = ["one", "two", "three"];
 
 const Navbar = () => {
+
+
   return (
     <>
       <nav>
-        <h1 className="navb">
+        <h1 className="navb" >
           <div>This is my navbar</div>
           <div className="stat">
             <StatusBar />
+          </div>
+          <div className="btn">
+            <button onClick={changeTheme}>change theme</button>
           </div>
         </h1>
 
