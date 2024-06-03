9b6d65189751205845d53a0761497d3e1a372c48
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-04 07:23:08 +0800

    d

---
+++ myredux/src/features/posts/postsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice} from '@reduxjs/toolkit'
+
+const initialState = [
+    {id: 1, title: 'first post', content: 'hello'},
+    {id: 2, title: 'second post', content: 'more text'}
+]
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded(state, action) {
+            state.push(action.payload)
+        }
+    }
+})
+
+export const {postAdded} = postSlice.actions
+
+export default postSlice.reducer
