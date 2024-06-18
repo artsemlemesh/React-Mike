0ca01c77c4f542d6087815401f374bb33c09e492
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-18 15:36:14 +0800

    d

--- CCarRent/backend/car_rent/car_rent/settings.py
+++ CCarRent/backend/car_rent/car_rent/settings.py
@@ -99,12 +99,12 @@ WSGI_APPLICATION = "car_rent.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
